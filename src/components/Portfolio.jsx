914c45b817541611d3520b36df12d536b54dbc32
nithyn1996 <nithyananthamganesan143@outlook.com>
2024-09-27 23:52:04 +0530

    Commit of portfolio changes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -13,8 +13,8 @@ const Portfolio = () => {
         {
             id: 1,
             src: arrayDestruct,
-            code:"",
-            demo:"",
+            code:"http://www.github.com",
+            demo:"http://www.github.com",
             
         },
         {
@@ -55,6 +55,10 @@ const Portfolio = () => {
         },
     ]
 
+    const openInNewTab = (url) => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+      };
+
   return (
     <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
@@ -68,12 +72,12 @@ const Portfolio = () => {
             </div>
             <div  className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
              {
-                portfolioData.map(({id, src}) => (
+                portfolioData.map(({id, src, code, demo}) => (
                     <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
                         <img src={src} alt="" className='rounded-md duration-200 hover:scale-105' />
                         <div className='flex items-center justify-center'>
-                            <button onClick={()=>{}} className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
-                            <button onClick={()=>{}} className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
+                            <button onClick={()=>openInNewTab(demo)} className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
+                            <button onClick={()=>openInNewTab(code)} className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
                         </div>
                     </div>
 
